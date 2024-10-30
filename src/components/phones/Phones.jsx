77ebe4541c407b069e5b5eb450ac10eca3deed91
jahidhasan6676 <jh18186676@gmail.com>
2024-10-30 08:32:01 +0600

    added the explore all file

--- src/components/phones/Phones.jsx
+++ src/components/phones/Phones.jsx
@@ -1,11 +1,13 @@
 import axios, { Axios } from "axios";
 import { useEffect, useState } from "react";
 import { BarChart, Bar, XAxis, YAxis, } from 'recharts';
-
+import { Audio } from 'react-loader-spinner'
+import DatePicker from "react-datepicker";
 
 const Phones = () => {
+    const [startDate, setStartDate] = useState(new Date())
     const [phone, setPhone] = useState([]);
-
+    const [loading, setLoading] = useState(true)
     useEffect(()=>{
         axios.get("https://openapi.programming-hero.com/api/phones?search=iphone")
         .then(data => {
@@ -19,6 +21,7 @@ const Phones = () => {
             })
          
             setPhone(phoneWithFakeData)
+            setLoading(false)
         })
         
 
@@ -26,6 +29,17 @@ const Phones = () => {
    
     return (
         <div>
+            <DatePicker></DatePicker>
+             {loading && <div className="flex justify-center">
+                <Audio
+                height="80"
+                width="80"
+                radius="9"
+                color="green"
+                ariaLabel="three-dots-loading"
+                wrapperStyle
+                wrapperClass
+            /></div>}
             <h2 className="text-2xl font-bold"> Phones: {phone.length}</h2>
             <BarChart width={1200} height={500} data={phone}>
                 <Bar dataKey={"slug"} fill="red"></Bar>
